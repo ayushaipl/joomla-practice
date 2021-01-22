2fd8e4e9957a30564978aa18ff66d6cbd98b6cce
ayushaipl <ayushraj.aipl@gmail.com>
2021-01-22 15:24:35 +0530

    added all files for trial

---
+++ logIn/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-01-21 10:55:16 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-01-21T10:55:16+00:00	INFO ::1	joomlacanceled	
